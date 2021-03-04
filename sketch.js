2ba57b68a273812de53a3bcf7e59f9295d2790f6
rvn276 <rvn276@gmail.com>
2021-03-04 10:08:03 +0530

    pro-c42

--- sketch.js
+++ sketch.js
@@ -1,138 +1,53 @@
-var monkey, monkey_running;
-var banana, bananaImage, obstacle, obstacleImage;
-var bananaGroup, obstacleGroup;
-var score, survialTime;
-var ground;
+var backImage,backgr;
+var player, player_running;
+var ground,ground_img;
 
-
-var PLAY = 1;
-var END = 0;
+var END =0;
+var PLAY =1;
 var gameState = PLAY;
 
-function preload() {
-
-
-  //Monkey
-  monkey_running = loadAnimation("sprite_0.png", "sprite_1.png", "sprite_2.png", "sprite_3.png", "sprite_4.png", "sprite_5.png", "sprite_6.png", "sprite_7.png", "sprite_8.png")
-  //Banana
-  bananaImage = loadImage("banana.png");
-  //Obstacle
-  obstacleImage = loadImage("obstacle.png");
+function preload(){
+  backImage=loadImage("jungle.jpg");
+  player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
 
 }
-
 
 function setup() {
-  createCanvas(400, 400);
-
-  bananaGroup = createGroup();
-  obstacleGroup = createGroup();
-  TimeGroup = createGroup();
-
-  monkey = createSprite(50, 250, 10, 10);
-  monkey.addAnimation("monkey", monkey_running);
-  monkey.scale = 0.1;
-
-  ground = createSprite(70, 350, 800, 10);
-  ground.velocityX = -4;
-  ground.x = ground.width / 2;
-
-  score = 0;
-  survialTime = 0;
-
+  createCanvas(800,400);
+  
+  backgr=createSprite(0,0,800,400);
+  backgr.addImage(backImage);
+  backgr.scale=1.5;
+  backgr.x=backgr.width/2;
+  backgr.velocityX=-4;
+  
+  player = createSprite(100,340,20,50);
+  player.addAnimation("Running",player_running);
+  player.scale = 0.1;
+  
+  ground = createSprite(400,350,800,10);
+  ground.x=ground.width/2;
+  ground.visible=false;
+  
 }
 
-function draw() {
+function draw() { 
+  background(0);
 
-  background(180);
-
-  stroke("black");
-  fill("black");
-  textSize(20);
-
-  text("Survial Time:" + survialTime, 100, 50);
-
-  stroke("black");
-  fill("black");
-  textSize(20);
-  text("Score:" + score, 300, 100);
-
-    monkey.collide(ground);
-    if (gameState === PLAY) {
-    monkey.changeAnimation("running", monkey_running);
-
-    survialTime = Math.ceil(frameCount / frameRate());
-
-
-    if (ground.x < 0) {
-      ground.x = ground.width / 2;
-    }
-
-      if (keyDown("space")) {
-      monkey.velocityY = -12;
-    }
-
-    if (bananaGroup.isTouching(monkey)) {
-      bananaGroup.destroyEach();
-      score = score + 1;
-    }
-
-    monkey.velocityY = monkey.velocityY + 0.8;
-    obstacleGroup.setLifetimeEach(-1);
-
-    food();
-    obstacles();
-
-
-    if (obstacleGroup.isTouching(monkey)) {
-
-      gameState = END;
-
-    }
+  if(gameState===PLAY){
+  
+  if(backgr.x<100){
+    backgr.x=backgr.width/2;
   }
   
-  if (gameState === END) {
-    obstacleGroup.destroyEach();
-    bananaGroup.destroyEach();
-    survialTime.visible = false;
-    ground.velocityX=0;
-
-    stroke("red");
-    fill("red");
-    textSize(30);
-    text("Game Over", 110, 200);
-
-    stroke("black");
-    fill("black");
-    textSize(30);
-    text("Monkey is dead", 100, 240);
-  }
+    if(keyDown("space") ) {
+      player.velocityY = -12;
+    }
+    player.velocityY = player.velocityY + 0.8;
   
+    player.collide(ground);
+
+  }
+
   drawSprites();
-}
-
-function food() {
-  if (frameCount % 150 === 0) {
-    banana = createSprite(400, 350, 40, 10);
-    banana.addImage(bananaImage);
-    banana.y = Math.round(random(120, 200));
-    banana.scale = 0.1;
-
-    banana.velocityX = -3;
-    banana.lifetime = 200;
-
-    bananaGroup.add(banana);
-  }
-}
-
-function obstacles() {
-  if (frameCount % 300 === 0) {
-    obstacle = createSprite(250, 325, 10, 10);
-    obstacle.addImage(obstacleImage);
-    obstacle.velocityX = -3;
-    obstacle.lifetime = 200;
-    obstacle.scale = 0.1;
-    obstacleGroup.add(obstacle);
-  }
-
 }
